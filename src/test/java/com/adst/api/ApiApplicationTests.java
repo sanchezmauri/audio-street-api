6b68d47357ac6ca0b8b811d39bfb9b539cc820cf
Mauricio <mrsanchez@baufest.com.ar>
2020-04-14 09:59:41 -0300

    se mockea el bean de repositorio

--- src/test/java/com/adst/api/ApiApplicationTests.java
+++ src/test/java/com/adst/api/ApiApplicationTests.java
@@ -3,11 +3,15 @@ package com.adst.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiApplicationTests {
+
+	@MockBean
+	private UsersRepository usersRepository;
 
 	@Test
 	public void contextLoads() {
